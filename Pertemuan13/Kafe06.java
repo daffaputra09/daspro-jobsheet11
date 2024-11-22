23acd1d4b65f1407b08aeaa7e2eb30218487085b
daffaputra09 <daffaputra863@gmail.com>
2024-11-22 09:41:10 +0700

    add hitungTotalHarga (percobaan 3)

--- Pertemuan13/Kafe06.java
+++ Pertemuan13/Kafe06.java
@@ -1,6 +1,21 @@
+package Pertemuan13;
+
+import java.util.Scanner;
+
 public class Kafe06 {
     public static void main(String[] args) {
         Menu("Andi", true, "DISKON50");
+
+         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System. out.print ("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+        
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+        
+        System. out.println("Total harga untuk pesanan Anda: Rp" + totalHarga); 
     }
 
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
@@ -18,14 +33,21 @@ public class Kafe06 {
             System.out.println("Kode promo invalid.");
         }
 
-        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("\n===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - Rp 15,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("============================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
     }
 }
