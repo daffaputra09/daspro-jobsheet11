171cd9d03d4d0cc296e15a5e2d377ec31d84878f
daffaputra09 <daffaputra863@gmail.com>
2024-11-22 08:48:56 +0700

    edit Kafe06 sesuai pertanyaan percobaan 2 nomor 5

--- Kafe06.java
+++ Kafe06.java
@@ -1,17 +1,26 @@
 public class Kafe06 {
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
     }
-    public static void Menu(String namaPelanggan, boolean isMember) {
+
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
-        
+
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
-        
+
+        if ("DISKON50".equals(kodePromo)) {
+            System.out.println("Kode promo diterima! Anda mendapatkan diskon 50%.");
+        } else if ("DISKON30".equals(kodePromo)) {
+            System.out.println("Kode promo diterima! Anda mendapatkan diskon 30%.");
+        } else {
+            System.out.println("Kode promo invalid.");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappuccino - Rp 20,000"); 
+        System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
