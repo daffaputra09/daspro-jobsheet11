e7ed4817068ef9cce7a50f627b4df15929435077
daffaputra09 <daffaputra863@gmail.com>
2024-11-23 10:34:57 +0700

    modify daftarPengunjung menggunakan foreach

--- Pertemuan13/PengunjungCafe06.java
+++ Pertemuan13/PengunjungCafe06.java
@@ -7,8 +7,8 @@ public class PengunjungCafe06 {
 
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
-    }
+        for (String pengunjung : namaPengunjung) {
+            System.out.println("- " + pengunjung);
+        }
     }
 }
